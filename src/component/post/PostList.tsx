159f673b8fc660de63d4b343912018ce144a1d7b
Jamie huang <lililala0112@gmail.com>
2024-09-04 09:27:21 +0800

    #01 Post fetch from data

---
+++ src/component/post/PostList.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import MainHeader from "./MainHeader";
+
+function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState<boolean>(true);
+  const [posts, setPosts] = useState<any[]>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const showModalHandler = () => {
+    setModalIsVisible(true);
+  };
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
+  const isPosting = () => {
+    showModalHandler();
+  };
+  const onStopPosting = () => {
+    hideModalHandler();
+  };
+  const addPostHandler = (postData) => {
+    setPosts((existingData) => [postData, ...existingData]);
+    console.log(posts);
+  };
+
+  useEffect(() => {
+    //load from Data
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
+    }
+    fetchPosts();
+  }, []);
+
+  return (
+    <div className="post-list">
+      <MainHeader onCreatePost={isPosting} />
+      {!isFetching && posts.length > 0 && (
+        <ul className="posts">
+          {posts.map((post) => (
+            <li>
+              <Post author={post.body} body={post.body} />
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts here!</h2>
+        </div>
+      )}
+
+      {isFetching && <p>Loading....</p>}
+
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost onCancel={onStopPosting} onSubmit={addPostHandler} />
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export default PostList;
